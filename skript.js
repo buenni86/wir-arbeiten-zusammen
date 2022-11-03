fd07b6291bf55890f3b54292567a720f15ad2437
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-11-03 12:07:20 +0100

    Update map

--- skript.js
+++ skript.js
@@ -1,4 +1,6 @@
-import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra"
+bootstrapExtra();
+//import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 
 var currentPopup = undefined;
 var isCoWebSiteOpened =  false;
